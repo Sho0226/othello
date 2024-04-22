05c9e3432e986792f251f18641133ccb57c891c3
Sho0226 <s1F102201616@iniad.org>
2024-04-23 08:25:11 +0900

    Third commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useSearchParams } from 'next/navigation';
 import styles from './index.module.css';
 import { useState } from 'react';
 
@@ -6,25 +5,49 @@ const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 0, 0, 1, 2],
+    [0, 0, 0, 0, 0, 2, 1, 2],
+    [0, 0, 0, 0, 1, 2, 1, 2],
+    [0, 0, 0, 2, 1, 2, 1, 2],
+    [0, 0, 1, 2, 1, 2, 1, 2],
+    [0, 1, 2, 1, 2, 1, 2, 1],
   ]);
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
     newBoard[y][x] = turnColor;
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+
+    if (board[y][x] === 0) {
+      if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
+        if (board[y + 2] !== undefined && board[y + 2][x] === turnColor) {
+          newBoard[y][x] = turnColor;
+          setTurnColor(3 - turnColor);
+          setBoard(newBoard);
+        } else if (board[y + 2] !== undefined && board[y + 2][x] === 3 - turnColor) {
+          if (board[y + 3] !== undefined && board[y + 3][x] === turnColor) {
+            newBoard[y][x] = turnColor;
+            setTurnColor(3 - turnColor);
+            setBoard(newBoard);
+          }
+        }
+      }
+
+      for (let i = 1; i < 8; i++) {
+        if (board[y + i] === undefined || board[y + i][x] === undefined || board[y + i][x] === 0) {
+          break;
+        } else if (board[y + i][x] === turnColor) {
+          newBoard[y][x] = turnColor;
+          setTurnColor(3 - turnColor);
+          setBoard(newBoard);
+          break;
+        } else if (board[y + i][x] === 3 - turnColor) {
+          continue;
+        }
+      }
     }
-    setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.boardstyle}>
