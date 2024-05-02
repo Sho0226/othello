861a2389ca1368c6738bfdf8aa9aaf060f729b35
Sho0226 <s1F102201616@iniad.org>
2024-05-02 21:14:15 +0900

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,6 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  console.log(1);
-
   const clickHandler = (x: number, y: number) => {
     console.log('今の座標は', x, y);
     const newBoard = structuredClone(board);
@@ -30,86 +28,76 @@ const Home = () => {
       [-1, 0], // Left
       [-1, 1], // Down-left
     ];
-    if (newBoard[y][x] === 3) {
-      console.log(10);
-      const updatedBoard = board.map((row) => row.map((cell) => (cell === 3 ? 0 : cell)));
+
+    if (newBoard[y][x] === 0) {
       for (const direction of directions) {
         const [x1, y1] = direction;
 
         for (let i = 1; i < 8; i++) {
-          const Y = y + y1 * i;
-          const X = x + x1 * i;
           //オセロを置くi個下の座標
-          if (updatedBoard[y + y1 * i] === undefined) {
+          if (board[y + y1 * i] === undefined) {
             //y座標の範囲外
             break;
-          } else if (updatedBoard[Y][X] === undefined) {
+          } else if (board[y + y1 * i][x + x1 * i] === undefined) {
             // x,y座標の範囲外
             break;
-          } else if (updatedBoard[Y][X] === 0) {
+          } else if (board[y + y1 * i][x + x1 * i] === 0) {
             // 置いてない座標
             break;
-          } else if (updatedBoard[Y][X] === turnColor) {
-            console.log(12);
+          } else if (board[y + y1 * i][x + x1 * i] === turnColor) {
             // 置いたオセロと同じ色
             if (i > 1) {
-              if (updatedBoard[Y][X] === updatedBoard[y + y1][x + x1]) {
+              if (board[y + y1 * i][x + x1 * i] === board[y + y1][x + x1]) {
                 // // 周囲の座標の色と周囲の座標の周りの色
                 break;
               } else {
                 for (let s = i; s >= 0; s--) {
-                  updatedBoard[y + y1 * s][x + x1 * s] = turnColor;
+                  newBoard[y + y1 * s][x + x1 * s] = turnColor;
                 }
                 setTurnColor(3 - turnColor);
-                setBoard(updatedBoard);
-                for (let i = 0; i < 8; i++) {
-                  for (let j = 0; j < 8; j++) {
-                    if (updatedBoard[i][j] === 0) {
-                      // 空のマスならば、次の手番で置けるかどうかを調べる
-                      let canPlace = false;
-                      for (const direction of directions) {
-                        const [x1, y1] = direction;
-                        let foundOpponent = false;
-                        for (let k = 1; k < 8; k++) {
-                          const newX = j + x1 * k;
-                          const newY = i + y1 * k;
+                setBoard(newBoard);
+                break;
+              }
+            }
+          } else if (board[y + y1 * i][x + x1 * i] === 3 - turnColor) {
+            //置いたオセロと異なる色
 
-                          // オセロを置くマスが盤面外の場合、ループを終了
-                          if (newX < 0 || newY < 0 || newX >= 8 || newY >= 8) break;
-                          console.log(2);
+            continue;
+          }
+        }
+      }
+    }
 
-                          if (updatedBoard[newY][newX] === 0 || updatedBoard[newY][newX] === 3) {
-                            // 空のマスか3のマスの場合、置ける
-                            if (updatedBoard[newY][newX] === 3) {
-                              foundOpponent = true;
-                              break;
-                            }
-                            break;
-                          } else if (updatedBoard[newY][newX] !== turnColor) {
-                            // 置いたオセロと同じ色の場合、逆算を終了
-                            if (foundOpponent) {
-                              canPlace = true;
-                            }
-                            break;
-                          } else {
-                            // 異なる色のオセロがある場合、次の方向を調べる
-                            foundOpponent = true;
-                          }
-                        }
-                        if (canPlace) break;
-                      }
-                      console.log(4);
-                      if (canPlace) updatedBoard[i][j] = 3; // 置ける場所を3に設定
-                    }
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (newBoard[i][j] === 0) {
+          for (const direction of directions) {
+            const [x1, y1] = direction;
+            for (let k = 1; k < 8; k++) {
+              const newX = j + x1 * k;
+              const newY = i + y1 * k;
+
+              if (newX < 0 || newY < 0 || newX >= 8 || newY >= 8) break;
+
+              if (board[newY][newX] === 0 || board[newY][newX] === 3) {
+                if (board[newY][newX] === 3) {
+                  break;
+                }
+                break;
+              } else if (board[newY][newX] === turnColor) {
+                if (k > 1) {
+                  if (board[newY][newX] === board[i + y1][j + x1]) {
+                    break;
+                  } else {
+                    board[newY][newX] === 3;
+                    setBoard(newBoard);
+                    break;
                   }
                 }
+              } else if (board[newY][newX] === 3 - turnColor) {
+                continue;
               }
-              break;
             }
-          } else if (board[Y][X] === 3 - turnColor) {
-            setBoard(newBoard);
-            console.log(14);
-            continue;
           }
         }
       }
@@ -117,7 +105,6 @@ const Home = () => {
   };
 
   const colorNum = (col: number) => board.flat().filter((c) => c === col).length;
-
   return (
     <div className={styles.container}>
       <>
